cb88ea200e574314e1bedde3a8fa9bf16f137ed8
alliance <alliancekubwayo@gmail,com>
2026-06-06 09:13:50 +0200

    docs: add GDP growth rate between 2022 and 2023 analytical query

--- storage/queries.sql
+++ storage/queries.sql
@@ -18,3 +18,27 @@ SELECT
     ROUND(((value - pib_2010) / pib_2010) * 100) AS growth_rate
 FROM growth
 WHERE year = 2023;
+
+
+
+-- GDP per capita growth rate by country between 2022 and 2023
+-- CTE: retrieves the 2022 value using LAG(1) for each country
+-- Main query: calculates the growth rate (%) on the 2023 row
+
+WITH year_growth AS (
+    SELECT 
+        c.name AS country_name, 
+        ec.year,
+        LAG(ec.value, 1) OVER (PARTITION BY country_id ORDER BY year) AS pib_2022, 
+        ec.value 
+    FROM fact_economic ec
+    JOIN dim_country c ON c.id = ec.country_id
+)
+SELECT 
+    country_name, 
+    pib_2022, 
+    value AS pib_2023, 
+    (value - pib_2022) AS growth,
+	ROUND(((value - pib_2022) / pib_2022) * 100) AS growth_rate
+FROM year_growth
+WHERE year = 2023 ORDER BY growth_rate DESC;
