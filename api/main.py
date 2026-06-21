6ae05cde654955bf0d940a9289748ec9a7709817
alliance <alliancekubwayo@gmail,com>
2026-06-21 12:37:10 +0200

    feat: add /gdp endpoint with pagination

--- api/main.py
+++ api/main.py
@@ -10,3 +10,17 @@ def country_list():
       SELECT code, name FROM dim_country;
     """
   )
+
+@app.get("/gdp")
+def country_gdp(
+  page: int = 1,
+  limit: int = 20):
+  OFFSET = (page - 1) * limit
+  return execute_query(
+    """
+      SELECT c.name AS country, ec.year, ec.value
+      FROM dim_country c
+      JOIN fact_economic ec ON c.id = ec.country_id
+      LIMIT %s OFFSET ap%s;
+    """, (limit, OFFSET)
+  )
