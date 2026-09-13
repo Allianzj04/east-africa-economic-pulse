cf6ddccfd3f2e761c8854c0204ef1f8a63e4cf5d
alliance <alliancekubwayo@gmail,com>
2026-09-13 19:59:05 +0200

    fix: use DATABASE_URL with urlparse in api/database.py to match storage/load.py

--- api/database.py
+++ api/database.py
@@ -1,18 +1,19 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
+import urllib.parse
 
 def get_connection():
-  config = {
-      "dbname": os.getenv("DB_NAME"),
-      "user": os.getenv("DB_USER"),
-      "password": os.getenv("DB_PASSWORD"),
-      "host": os.getenv("DB_HOST"),
-      "port": os.getenv("DB_PORT")
-  }
-  conn = psycopg2.connect(**config)
+  load_dotenv()
+  database_url = os.getenv('DATABASE_URL')
+  parsed = urllib.parse.urlparse(database_url)
+  conn = psycopg2.connect(
+    dbname=parsed.path[1:],
+    host=parsed.hostname,
+    user=parsed.username,
+    password=parsed.password,
+    port=parsed.port,
+  )
   return conn
 
 def execute_query(sql, params=None):
