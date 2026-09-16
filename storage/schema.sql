1287eb48a5d72d6881a6fa346fb2f876e05b5141
alliance <alliancekubwayo@gmail,com>
2026-09-16 21:31:15 +0200

    fix: correct foreign key referencing wrong table

--- storage/schema.sql
+++ storage/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS dim_indicator (
 CREATE TABLE IF NOT EXISTS fact_economic (
   id SERIAL PRIMARY KEY,
   country_id INTEGER REFERENCES dim_country(id),
-  indicator_id INTEGER REFERENCES dim_country(id),
+  indicator_id INTEGER REFERENCES dim_indicator(id),
   year INTEGER NOT NULL,
   value NUMERIC(15,2),
   UNIQUE(country_id, indicator_id, year)
