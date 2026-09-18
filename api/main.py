3bdd65b6226cfd4668483a4c8e6731cd5d7de3c6
alliance <alliancekubwayo@gmail,com>
2026-09-18 19:30:17 +0200

    fix: filter analytical endpoints and ranking query by indicator

--- api/main.py
+++ api/main.py
@@ -34,18 +34,26 @@ def country_gdp(
   )
 
 @app.get("/gdp/{country_code}")
-def country_code_gdp(country_code: str):
-  return execute_query(
-    """
-      SELECT c.name AS country, ec.year, ec.value
-      FROM dim_country c
-      JOIN fact_economic ec ON c.id = ec.country_id
-      WHERE c.code = %s;
-    """, (country_code,)
+def get_indicator_by_country(
+  country_code: str,
+  indicator: str = "gdp_per_capita"
+  ):
+    return execute_query(
+      """
+        SELECT c.name AS country, ec.year, ec.value, di.label
+        FROM dim_country c
+        JOIN fact_economic ec ON c.id = ec.country_id
+	      JOIN dim_indicator di ON ec.indicator_id = di.id
+        WHERE c.code = %s AND di.label = %s;
+      """, 
+      (country_code, indicator)
   )
+
 @app.get("/countries/ranking")
 def country_ranking(
-  year: int = 2023):
+  year: int = 2023,
+  indicator: str = "gdp_per_capita"
+  ):
   return execute_query(
     """
 WITH ranking AS (
@@ -56,12 +64,13 @@ WITH ranking AS (
         RANK() OVER (ORDER BY value DESC) AS rank
     FROM fact_economic ec
     JOIN dim_country c ON c.id = ec.country_id
-	WHERE year = %s
+	  JOIN dim_indicator di ON ec.indicator_id = di.id
+	  WHERE year = %s AND di.label = %s
 )
 SELECT 
     country_name, 
     value, 
-	rank
+	  rank
 FROM ranking;
-    """, (year, )
+    """, (year, indicator)
   )
--- storage/ranking_query.sql
+++ storage/ranking_query.sql
@@ -1,6 +1,6 @@
 -- GDP per capita ranking by country in 2023
--- CTE: calculates the rank by GDP per capita in 2023 using RANK()
--- Main query: show the ranking of the 5 nations
+-- CTE: calculates the rank by GDP per capita (indicator label = 'gdp_per_capita') in 2023 using RANK()
+-- Main query: shows the ranking of countries based on the gdp_per_capita indicator
 
 WITH ranking AS (
     SELECT 
@@ -10,7 +10,8 @@ WITH ranking AS (
         RANK() OVER (ORDER BY value DESC) AS rank
     FROM fact_economic ec
     JOIN dim_country c ON c.id = ec.country_id
-	WHERE year = 2023
+	JOIN dim_indicator di ON ec.indicator_id = di.id
+	WHERE year = 2023 AND di.label = 'gdp_per_capita'
 )
 SELECT 
     country_name, 
